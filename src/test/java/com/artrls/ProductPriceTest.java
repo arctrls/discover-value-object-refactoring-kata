6b452b83818fb910290ef59c3fa513ea8b8d71ed
Kiwon Lee <jake@ktown4u.com>
2025-08-08 15:40:49 +0900

    27. (test coverage 100%) add a test case for discountRate.

--- src/test/java/com/artrls/ProductPriceTest.java
+++ src/test/java/com/artrls/ProductPriceTest.java
@@ -27,11 +27,25 @@ class ProductPriceTest {
         );
     }
 
+    @Test
+    void discountRate() {
+        assertAll(
+                () -> assertDiscountRate(1000L, 0L, 0),
+                () -> assertDiscountRate(1000L, 900L, 10),
+                () -> assertDiscountRate(1000L, 1000L, 0),
+                () -> assertDiscountRate(1000L, 1100L, 0)
+        );
+    }
+
     private void assertIsDiscounted(final long originalPrice, final long discountedPrice, final boolean expected) {
         assertThat(new ProductPrice(originalPrice, discountedPrice).isDiscounted()).isEqualTo(expected);
     }
 
     private void assertFinalPrice(final long originalPrice, final long discountedPrice, final long expected) {
         assertThat(new ProductPrice(originalPrice, discountedPrice).finalPrice()).isEqualTo(expected);
+    }
+
+    private void assertDiscountRate(final long originalPrice, final long discountedPrice, final int expected) {
+        assertThat(new ProductPrice(originalPrice, discountedPrice).discountRate()).isEqualTo(expected);
     }
 }
